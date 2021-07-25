b856b87dc94b279209311d7ca49e8bf77fe9cece
Michael Kim <59414764+xcellentbird@users.noreply.github.com>
2021-07-25 23:21:57 +0900

    Add files via upload

---
+++ config/settings/local.py
@@ -0,0 +1,5 @@
+# base.py(settings.py)파일의 모든 내용을 사용한다.
+from .base import *
+
+# HOST만 다르게 적용하기 위한 list
+ALLOWED_HOSTS = []
---
+++ config/settings/prod.py
@@ -0,0 +1,5 @@
+from .base import *
+
+# 보통 운영 환경을 production 환경이라고 한다.
+
+ALLOWED_HOSTS = ['13.209.181.186']
